dba1e0b3f99ac8c9a2b0a1b515670864153ab8ab
Nicholas Mayne <nick@modelrocket.io>
2026-05-07 14:37:18 -0700

    add universe to day7

--- day7/code.py
+++ day7/code.py
@@ -17,6 +17,36 @@ class tachyon:
             ret_val += '\n'
         return ret_val
 
+    def split(self) -> int:
+        splits = 0
+        current_beams: dict[int, int] = {}
+
+        for y in sorted(self.manifolds):
+            for x in sorted(self.manifolds[y]):
+                if self.manifolds[y][x] == 'S':
+                    current_beams[x] = 1
+                elif self.manifolds[y][x] == '^':
+                    if x in current_beams:
+                        if x+1 in current_beams:
+                            current_beams[x + 1] += current_beams[x]
+                        else:
+                            current_beams[x + 1] = current_beams[x]
+                        if x-1 in current_beams:
+                            current_beams[x - 1] += current_beams[x]
+                        else:
+                            current_beams[x - 1] = current_beams[x]
+
+                        del current_beams[x]
+                        splits += 1
+                        print(f'split at [y,x]=[{y},{x}]')
+            print(
+                f'current={[f"{item}[{current_beams[item]}]" for item in sorted(current_beams)]}')
+            print(f'splits={splits}')
+            print(
+                f'universes={sum([current_beams[item] for item in current_beams])}')
+        return splits
+
 
 my_tachyon = tachyon("day7/data.txt")
 print(my_tachyon)
+print(f'split={my_tachyon.split()}')
